9696b0295a034ff7a16709b169ac42dcbe6fb5fe
eunchae-Cho <gnltj37@naver.com>
2021-02-27 20:48:07 +0900

    commit

--- app/routes/room/room.js
+++ app/routes/room/room.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var con = require('../../config/mysql');
 
+// 숙소 전체 불러오기
 router.get('/', (request, response) => {
-    response.render('./room/roomList');
+    con.query('SELECT RNO, HNAME, IMAGE, PRICE, CAPA, HCONTENT FROM ROOM', (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            for(var i=0; i<result.length; i++) {
+                result[i].PRICE = result[i].PRICE.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            }
+            response.render('./room/roomList', {result:result});
+        }
+    });
+});
+
+// 숙소 상세보기
+router.get('/detail/:rno', (request, response) => {
+    var rno = request.params.rno;
+    con.query(`SELECT * FROM ROOM WHERE RNO=?`,[rno], (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            result[0].PRICE = result[0].PRICE.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            response.render('./room/roomDetail',{result:result});
+        }
+    });
 });
 
 module.exports = router;
